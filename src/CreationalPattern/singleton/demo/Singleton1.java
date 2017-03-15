cd16982eb10131495d332e5fe168fc4c339f481b
yangenneng <1274491995@qq.com>
2017-03-15 09:46:07 +0800

    简单单例模式

---
+++ src/CreationalPattern/singleton/demo/Singleton1.java
@@ -0,0 +1,15 @@
+package CreationalPattern.singleton.demo;
+
+/**
+ * Created by yangenneng on 2017-03-15 9:40.
+ * 功能说明：饿汉式单例模式
+ */
+public class Singleton1 {
+    private static Singleton1 singleton1_instace=new Singleton1();
+
+    private Singleton1(){}
+
+    public static Singleton1 getSingleton1_instace() {
+        return singleton1_instace;
+    }
+}
---
+++ src/CreationalPattern/singleton/demo/Singleton2.java
@@ -0,0 +1,15 @@
+package CreationalPattern.singleton.demo;
+
+/**
+ * Created by yangenneng on 2017-03-15 9:42.
+ * 功能说明：懒汉式单例模式
+ */
+public class Singleton2 {
+    private static Singleton2 singleton2_instance=null;
+    private Singleton2(){}
+    public static Singleton2 getSingleton2_instance() {
+        if(singleton2_instance==null)
+           singleton2_instance=new Singleton2();
+        return singleton2_instance;
+    }
+}
